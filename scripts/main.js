1612dd04fdaa80ebe61711c712080a638e117f06
maximobelen <maximobelen@me.com>
2015-09-26 01:10:58 -0300

    add animations

---
+++ scripts/main.js
@@ -0,0 +1,32 @@
+var init = function(){
+  var name = document.getElementsByClassName('name')[0];
+  var job = document.getElementsByClassName('job')[0];
+  var image = document.getElementsByClassName('image')[0];
+  var background = document.getElementsByClassName('avatar')[0];
+
+  TweenMax.fromTo(name, 0.4, {autoAlpha:0, y: 50}, {delay: 0.4, autoAlpha:1, y:0, ease: Power2.easeOut});
+  TweenMax.fromTo(job, 0.4, {autoAlpha:0, y: 50}, {delay: 0.6, autoAlpha:1, y:0, ease: Power2.easeOut});
+  TweenLite.fromTo(image, 0.8,  {autoAlpha:0, scaleX:0.2, scaleY:0.2}, 
+    {autoAlpha:1, delay: 0.6, scaleX:1, scaleY:1, ease: Power2.easeOut});
+
+  TweenLite.fromTo(background, 0.8,  {autoAlpha:0, scaleX:0.2, scaleY:0.2}, 
+    {autoAlpha:1, delay: 0.6, scaleX:1, scaleY:1, ease: Power2.easeOut});
+
+
+  addListeners();
+};
+
+var addListeners =  function(){
+  var image = document.getElementsByClassName('image')[0];
+  var background = document.getElementsByClassName('avatar')[0];
+
+  image.addEventListener("mouseenter", function(){
+    TweenLite.to(image, 0.4, {autoAlpha:0.9, ease: Power2.easeOut});
+    TweenLite.to(background, 0.4, {scaleX:1.1, scaleY:1.1, ease: Power2.easeOut});
+  }.bind(this), false);
+
+  image.addEventListener("mouseleave", function(){
+    TweenLite.to(image, 0.4, {autoAlpha:1, scaleX:1, scaleY:1, ease: Power2.easeOut});
+    TweenLite.to(background, 0.4, {scaleX:1, scaleY:1, ease: Power2.easeOut});
+  }.bind(this), false);
+};
